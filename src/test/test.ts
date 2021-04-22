d34541c43433ba223df7b044f40e202a239251d4
Max Mustermann <maxmustermann@disroot.org>
2021-04-22 15:45:44 +0530

    Split tests

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,6 +1,6 @@
 const verify = require( "../verification/index" ).codeVerification
 
-test('Running tests', async () => {
+test('Running test 1', async () => {
   let data = await verify(
     {
       contractAddress: 'one1rcs4yy4kln53ux60qdeuhhvpygn2sutn500dhw',
@@ -11,7 +11,10 @@ test('Running tests', async () => {
     }
   )
   expect(data.verified).toBe(true)    // will match
-  data = await verify(
+});
+
+test('Running test 2', async () => {
+  let data = await verify(
     {
       contractAddress: 'one1rcs4yy4kln53ux60qdeuhhvpygn2sutn500dhw',
       githubURL: 'https://github.com/rachit2501/Lottery-System/blob/master/contracts/Lottery.sol',
@@ -20,7 +23,10 @@ test('Running tests', async () => {
     }
   )
   expect(data.verified).toBe(false)   // contract was compiled with 0.4.17 but this will provide coverage for no metadata extraction portion
-  data = await verify(
+});
+
+test('Running test 3', async () => {
+  let data = await verify(
     {
       contractAddress: 'one1rcs4yy4kln53ux60qdeuhhvpygn2sutn500dhwa',
       githubURL: 'https://github.com/rachit2501/Lottery-System/blob/master/contracts/Lottery.sol',
@@ -29,26 +35,35 @@ test('Running tests', async () => {
       keep: true
     }
   )
-  expect(data.verified).toBe(false)   // invalid address
-  data = await verify(
+  expect(data.verified).toBe(false)   // invalid Bech32 address
+});
+
+test('Running test 4', async () => {
+  let data = await verify(
     {
-      contractAddress: 'one105z5dka3mj5pprve4guf4r5uurzqkgms40ma3e',
+      contractAddress: 'one16skweach8tqt0ce4a3x8jtuz0gtrn374vul3ua',
       githubURL: 'https://github.com/rachit2501/Lottery-System/blob/master/contracts/Lottery.sol',
-      chainType: 'mainnet',
+      chainType: 'testnet',
       solidityVersion: '0.4.17',
     }
   )
-  expect(data.verified).toBe(false)   // different contract on mainnet
-  data = await verify(
+  expect(data.verified).toBe(false)   // different contract (MetaCoin.sol) from the one posted at the above address
+});
+
+test('Running test 5', async () => {
+  let data = await verify(
     {
       contractAddress: 'one1rcs4yy4kln53ux60qdeuhhvpygn2sutn500dhw',
       githubURL: 'Fake',
-      chainType: 'mainnet',
+      chainType: 'testnet',
       solidityVersion: '0.4.17'
     }
   )
   expect(data.verified).toBe(false)   // Github.ts failure
-  data = await verify(
+});
+
+test('Running test 6', async () => {
+  let data = await verify(
     {
       contractAddress: 'one1rcs4yy4kln53ux60qdeuhhvpygn2sutn500dhw',
       githubURL: 'Fake.sol',

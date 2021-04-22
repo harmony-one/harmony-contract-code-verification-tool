db7dedc4246afefd9dc656d8c1724d2de998ee70
Max Mustermann <maxmustermann@disroot.org>
2021-04-22 15:46:12 +0530

    Return full response from RPC instead of result only

--- src/verification/rpc.ts
+++ src/verification/rpc.ts
@@ -1,26 +1,26 @@
 const { Harmony } = require("@harmony-js/core")
 const { ChainID, ChainType } = require("@harmony-js/utils")
 
-const rpcUrl = {
+let rpcUrl = {
   'testnet':"https://api.s0.b.hmny.io/",
   'mainnet':"https://api.s0.t.hmny.io/",
 }
 
-const testnet = new Harmony(rpcUrl.testnet, {
+let testnet = new Harmony(rpcUrl.testnet, {
   chainType: ChainType.Harmony,
   chainId: ChainID.HmyTestnet,
 })
 
-const mainnet = new Harmony(rpcUrl.mainnet, {
+let mainnet = new Harmony(rpcUrl.mainnet, {
   chainType: ChainType.Harmony,
   chainId: ChainID.HmyMainnet,
 })
 
-export const getSmartContractCode = async (chain, address) => {
+export let getSmartContractCode = async (chain, address) => {
   const hmy = chain === 'mainnet' ? mainnet : testnet
   const response = await hmy.blockchain.getCode({
     address,
     blockNumber: "latest",
   })
-  return response.result
+  return response
 }
